e296be5728843a0cf2c8787dce3ec177c0d181dd
Gong Zhang <zhanggong@me.com>
2020-03-28 17:24:46 +0800

    Upgrade pandoc to the latest version

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,11 @@
-FROM ubuntu:16.04 AS builder
-COPY sources.list /etc/apt/sources.list
+FROM pandoc/core:2.9.2
+RUN apk add --update --no-cache ttf-dejavu
 
-RUN apt-get update && \
-    apt-get install -y fonts-dejavu-core git pandoc unzip zip wget && \
-    rm -rf /var/lib/apt/lists/*
+WORKDIR /root
+COPY . .
 
-WORKDIR /root/You-Dont-Know-JS
-COPY You-Dont-Know-JS /root/You-Dont-Know-JS
-COPY covers ./covers
-COPY epub.css ./
-COPY gen.sh ./
-RUN chmod +x gen.sh && mkdir output && ./gen.sh
+RUN chmod +x gen.sh
 
-FROM python:3-alpine
-WORKDIR /root/epub
-COPY --from=builder /root/You-Dont-Know-JS/output/*.epub /root/epub/
 EXPOSE 80
-CMD python3 -m http.server 80
+ENTRYPOINT []
+CMD ./gen.sh
--- gen.sh
+++ gen.sh
@@ -3,21 +3,39 @@
 # NOTE: this file is a modified version of gist:
 # https://gist.github.com/bmaupin/6e3649af73120fac2b6907169632be2c
 
+ROOT_DIR=$PWD
+FONT_PATH=/usr/share/fonts/ttf-dejavu/DejaVuSansMono.ttf
+
+TITLE="You Don't Know JS Yet"
+AUTHOR="Kyle Simpson"
+
+BOOK1="Get Started"
+BOOK2="Scope & Closures"
+BOOK3="Objects & Classes"
+BOOK4="Types & Grammar"
+BOOK5="Sync & Async"
+BOOK6="ES.Next & Beyond"
+
+echo "Preparing..."
+
 # Clean up redundant headings that end up getting split into separate chapters by themselves
 find . -iname "*.md" -exec sed -i '/# You Don'\''t Know JS.*/d' {} \;
 # Fix <br> tags which pandoc won't convert to <br />
-find . -iname "*.md" -exec sed -i 's#<br>#<br />#' {} \;
+find . -iname "*.md" -exec sed -i 's#<br>#<br />#g' {} \;
 # Close img tags
 find . -iname "*.md" -exec sed -i -r 's#(<img.*[^/])>#\1 />#' {} \;
 
+cd 2nd-edition
+
+echo "Generating: $BOOK1..."
 cd get-started
-pandoc -S -o ../output/"You Don't Know JS - Get Started.epub" \
-  --epub-cover-image=./images/cover.png \
-  --epub-embed-font=/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf \
-  --epub-stylesheet=../epub.css \
-  -M author="Kyle Simpson" \
-  -M title="You Don't Know JS: Get Started" \
-  --columns=1000 \
+pandoc -f markdown+smart -o "$ROOT_DIR/output/$TITLE - $BOOK1.epub" \
+  --epub-cover-image=images/cover.png \
+  --epub-embed-font=$FONT_PATH \
+  --css=$ROOT_DIR/epub.css \
+  --no-highlight \
+  -M author="$AUTHOR" \
+  -M title="$TITLE: $BOOK1" \
   foreword.md \
   ../preface.md \
   ch1.md \
@@ -28,14 +46,50 @@ pandoc -S -o ../output/"You Don't Know JS - Get Started.epub" \
   apB.md
 cd ..
 
+echo "Generating: $BOOK2..."
 cd scope-closures
-pandoc -S -o ../output/"You Don't Know JS - Scope & Closures.epub" \
-  --epub-cover-image=../covers/scope-closures.jpg \
-  --epub-embed-font=/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf \
-  --epub-stylesheet=../epub.css \
-  -M author="Kyle Simpson" \
-  -M title="You Don't Know JS: Scope & Closures" \
-  --columns=1000 \
+pandoc -f markdown+smart -o "$ROOT_DIR/output/$TITLE - $BOOK2.epub" \
+  --epub-cover-image=images/cover.png \
+  --epub-embed-font=$FONT_PATH \
+  --css=$ROOT_DIR/epub.css \
+  --no-highlight \
+  -M author="$AUTHOR" \
+  -M title="$TITLE: $BOOK2" \
+  foreword.md \
+  ../preface.md \
+  ch1.md \
+  ch2.md \
+  ch3.md \
+  ch4.md \
+  ch5.md \
+  ch6.md \
+  ch7.md \
+  ch8.md \
+  apA.md \
+  apB.md
+cd ..
+
+
+# ==== NOTE: the 4 books below are from the 1st edition ====
+
+cd $ROOT_DIR/1st-edition
+
+TITLE="You Don't Know JS"
+
+BOOK3="this & Object Prototypes"
+BOOK4="Types & Grammar"
+BOOK5="Async & Performance"
+BOOK6="ES6 & Beyond"
+
+echo "Generating: $BOOK3..."
+cd "this & object prototypes"
+pandoc -f markdown+smart -o "$ROOT_DIR/output/$TITLE - $BOOK3.epub" \
+  --epub-cover-image=cover.jpg \
+  --epub-embed-font=$FONT_PATH \
+  --css=$ROOT_DIR/epub.css \
+  --no-highlight \
+  -M author="$AUTHOR" \
+  -M title="$TITLE: $BOOK3" \
   foreword.md \
   ../preface.md \
   ch1.md \
@@ -48,14 +102,15 @@ pandoc -S -o ../output/"You Don't Know JS - Scope & Closures.epub" \
   apB.md
 cd ..
 
-cd objects-classes
-pandoc -S -o ../output/"You Don't Know JS - Object & Classes.epub" \
-  --epub-cover-image=../covers/this-object-prototypes.jpg \
-  --epub-embed-font=/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf \
-  --epub-stylesheet=../epub.css \
-  -M author="Kyle Simpson" \
-  -M title="You Don't Know JS: Object & Classes" \
-  --columns=1000 \
+echo "Generating: $BOOK4..."
+cd "types & grammar"
+pandoc -f markdown+smart -o "$ROOT_DIR/output/$TITLE - $BOOK4.epub" \
+  --epub-cover-image=cover.jpg \
+  --epub-embed-font=$FONT_PATH \
+  --css=$ROOT_DIR/epub.css \
+  --no-highlight \
+  -M author="$AUTHOR" \
+  -M title="$TITLE: $BOOK4" \
   foreword.md \
   ../preface.md \
   ch1.md \
@@ -63,36 +118,19 @@ pandoc -S -o ../output/"You Don't Know JS - Object & Classes.epub" \
   ch3.md \
   ch4.md \
   ch5.md \
-  ch6.md \
-  apA.md
+  apA.md \
+  apB.md
 cd ..
 
-cd types-grammar
-pandoc -S -o ../output/"You Don't Know JS - Types & Grammar.epub" \
-  --epub-cover-image=../covers/types-grammar.jpg \
-  --epub-embed-font=/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf \
-  --epub-stylesheet=../epub.css \
-  -M author="Kyle Simpson" \
-  -M title="You Don't Know JS: Types & Grammar" \
-  --columns=1000 \
-  foreword.md \
-  ../preface.md \
-  ch1.md \
-  ch2.md \
-  ch3.md \
-  ch4.md \
-  ch5.md \
-  apA.md
-cd ..
-
-cd sync-async
-pandoc -S -o ../output/"You Don't Know JS - Sync & Async.epub" \
-  --epub-cover-image=../covers/async-performance.jpg \
-  --epub-embed-font=/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf \
-  --epub-stylesheet=../epub.css \
-  -M author="Kyle Simpson" \
-  -M title="You Don't Know JS: Sync & Async" \
-  --columns=1000 \
+echo "Generating: $BOOK5..."
+cd "async & performance"
+pandoc -f markdown+smart -o "$ROOT_DIR/output/$TITLE - $BOOK5.epub" \
+  --epub-cover-image=cover.jpg \
+  --epub-embed-font=$FONT_PATH \
+  --css=$ROOT_DIR/epub.css \
+  --no-highlight \
+  -M author="$AUTHOR" \
+  -M title="$TITLE: $BOOK5" \
   foreword.md \
   ../preface.md \
   ch1.md \
@@ -102,17 +140,19 @@ pandoc -S -o ../output/"You Don't Know JS - Sync & Async.epub" \
   ch5.md \
   ch6.md \
   apA.md \
-  apB.md
+  apB.md \
+  apC.md
 cd ..
 
-cd es-next-beyond
-pandoc -S -o ../output/"You Don't Know JS - ES Next & Beyond.epub" \
-  --epub-cover-image=../covers/es-next-beyond.jpg \
-  --epub-embed-font=/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf \
-  --epub-stylesheet=../epub.css \
-  -M author="Kyle Simpson" \
-  -M title="You Don't Know JS: ES: Next & Beyond" \
-  --columns=1000 \
+echo "Generating: $BOOK6..."
+cd "es6 & beyond"
+pandoc -f markdown+smart -o "$ROOT_DIR/output/$TITLE - $BOOK6.epub" \
+  --epub-cover-image=cover.jpg \
+  --epub-embed-font=$FONT_PATH \
+  --css=$ROOT_DIR/epub.css \
+  --no-highlight \
+  -M author="$AUTHOR" \
+  -M title="$TITLE: $BOOK6" \
   foreword.md \
   ../preface.md \
   ch1.md \
@@ -126,15 +166,4 @@ pandoc -S -o ../output/"You Don't Know JS - ES Next & Beyond.epub" \
   apA.md
 cd ..
 
-# Workaround for https://github.com/jgm/pandoc/issues/3792
-cd output
-for epubfile in *.epub; do
-    mkdir tmp-epub
-    cd tmp-epub
-    unzip ../"$epubfile"
-    sed -i 's/ & / \&amp; /' title_page.xhtml
-    zip -X ../"$epubfile" mimetype
-    zip -rX ../"$epubfile" * -x mimetype
-    cd ..
-    rm -rf tmp-epub
-done
+echo "Done."
